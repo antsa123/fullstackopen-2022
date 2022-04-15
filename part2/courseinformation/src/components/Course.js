e1024392417e37aefb5645ed034c3c662b29efca
aparkkonen <antti.parkkonen@navitec.fi>
2022-04-15 14:10:14 +0300

    Part steps 2.1 -> 2.3

---
+++ part2/courseinformation/src/components/Course.js
@@ -0,0 +1,45 @@
+const Header = ({course}) => {
+    return (
+      <h1>{course}</h1>
+    )
+  }
+  const Content = ({parts}) => {
+    return (
+      <>
+        {
+            parts.map(part => <Part key={part.id} part={part.name} exercise={part.exercises}/>)
+        }
+      </>
+    )
+  }
+  
+  const Part = ({part, exercise}) => {
+    return (
+      <>
+        <p>
+          {part} {exercise}
+        </p>
+      </>
+    )
+  }
+  
+  const Total = ({parts}) => {
+    return (
+      <>
+        <p><b>Number of exercises {parts.reduce((n, {exercises}) => n + exercises, 0)}</b></p>
+      </>
+    )
+  }
+
+
+const Course = ({course}) => {
+    return (
+      <div>
+        <Header course={course.name}/>
+        <Content parts={course.parts}/>
+        <Total parts={course.parts}/>
+      </div>
+    )
+  }
+
+export default Course
