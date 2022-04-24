4099ff8efd686d6363ea071f7fef3e6a88f5a180
aparkkonen <antti.parkkonen@navitec.fi>
2022-04-24 15:29:52 +0300

    Fetching initial data from the server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,13 +1,19 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import PeopleList from './components/PeopleList'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1231244'}
-  ]) 
+  const [persons, setPersons] = useState([])
+
+  useEffect(() =>
+  axios.get('http://localhost:3001/persons').then(response => {
+    setPersons(response.data)
+  })
+  , [])
+  
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
