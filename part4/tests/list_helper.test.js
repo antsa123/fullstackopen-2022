8ddc6fd1aae28cd5b2ac6edce393c0e366d33046
aparkkonen <antti.parkkonen@navitec.fi>
2022-06-03 14:45:06 +0300

    More helpers and unit tests

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -1,29 +1,53 @@
+const blog = require('../models/blog')
 const listHelper = require('../utils/list_helper')
+const blogInput = require('./blog_test_inputs')
+
+describe('most blogs', () => {
+  test('return author with most blogs and the number of blogs', () =>  {
+    const result = listHelper.authorWithMostBlogsAndBlogCount(blogInput)
+    expect(result.author).toBe('Robert C. Martin')
+    expect(result.blogs).toBe(3)
+  })
+})
+
+describe('most liked author', () => {
+  test('return the author whose blogs have most likes', () => {
+    const result = listHelper.mostLikedAuthorAndLikeCount(blogInput)
+    expect(result.author).toBe('Edsger W. Dijkstra')
+    expect(result.likes).toBe(17)
+  })
+})
+
 describe('total likes', () => {
-  const blogInput = require('./blog_test_inputs')
-
-
   test('total likes with multiple liked blogs', () => {
-    const result = listHelper.totalLikes(blogInput)
+    const result = listHelper.totalLikesOfAllBlogs(blogInput)
     expect(result).toBe(36)
   })
 
   test('total likes with empty input', () => {
-    const result = listHelper.totalLikes([])
+    const result = listHelper.totalLikesOfAllBlogs([])
     expect(result).toBe(0)
   })
 
   test('total with only one input with likes', () => {
-    const result = listHelper.totalLikes([blogInput[0]])
+    const result = listHelper.totalLikesOfAllBlogs([blogInput[0]])
     expect(result).toBe(blogInput[0].likes)
   })
 
   test('total with multiple non liked blogs', () => {
     const blogsWith0Likes = blogInput.map(blog => {
-      blog.likes = 0
-      return blog
+      const blogWith0likes = {...blog, likes: 0}
+      return blogWith0likes
     })
-    const result = listHelper.totalLikes(blogsWith0Likes)
+    const result = listHelper.totalLikesOfAllBlogs(blogsWith0Likes)
     expect(result).toBe(0)
+  })
+})
+
+describe('most likes', () => {
+  test('return favorite blog', () => {
+    const result = listHelper.blogWithMostLikes(blogInput)
+    expect(result.likes).toBe(12)
+    expect(result.title).toBe('Canonical string reduction')
   })
 })
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,7 +1,63 @@
-const totalLikes = (blogs) => {
+const totalLikesOfAllBlogs = (blogs) => {
   return blogs.reduce((sum, item) => { return sum + item.likes }, 0)
 }
 
+const blogWithMostLikes = (blogs) => {
+  return blogs.reduce((previousWithMaxLikes, candidateForMaxLikes) => {
+    return (previousWithMaxLikes.likes > candidateForMaxLikes.likes) ? previousWithMaxLikes : candidateForMaxLikes
+  })
+}
+
+const authorWithMostBlogsAndBlogCount = (blogs) => {
+  let writers = []
+  blogs.forEach((currentBlog) => {
+    let writerToUpdate = writers.find(writer => writer.author === currentBlog.author)
+    if (writerToUpdate)
+    {
+      writerToUpdate.blogs += 1
+      return
+    }
+
+    writers.push(
+      {
+        'author': currentBlog.author,
+        'blogs': 1
+      })
+  })
+
+  return writers.reduce((previousWithMostBlogs, candidateForMostBlogs) => {
+    return (previousWithMostBlogs.blogs > candidateForMostBlogs.blogs) ? previousWithMostBlogs : candidateForMostBlogs
+  })
+}
+
+const mostLikedAuthorAndLikeCount = (blogs) => {
+  let writers = []
+  blogs.forEach((currentBlog) => {
+    let writerToUpdate = writers.find(writer => writer.author === currentBlog.author)
+    if (writerToUpdate)
+    {
+      writerToUpdate.likes += currentBlog.likes
+      return
+    }
+
+    writers.push(
+      {
+        'author': currentBlog.author,
+        'likes': currentBlog.likes
+      })
+  })
+
+  console.log(writers)
+
+  return writers.reduce((previousWithMostLikes, candidateForMostLikes) => {
+    return (previousWithMostLikes.likes > candidateForMostLikes.likes) ? previousWithMostLikes : candidateForMostLikes
+  })
+}
+
+
 module.exports = {
-  totalLikes
+  totalLikesOfAllBlogs,
+  blogWithMostLikes,
+  authorWithMostBlogsAndBlogCount,
+  mostLikedAuthorAndLikeCount
 }
