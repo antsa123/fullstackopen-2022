e63307cd2f9cca5de999415f1a021ed30b0f1a84
aparkkonen <antti.parkkonen@navitec.fi>
2022-12-11 17:29:11 +0200

    ex 6.14 using json-server to store the anecdotes and axios to fetch or update them.

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,14 +1,16 @@
 import { useDispatch } from "react-redux";
 import { createAnecdote } from "../reducers/anecdoteReducer";
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = (props) => {
   const dispatch = useDispatch()
 
-  const create = (event) => {
+  const create = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ""
-    dispatch(createAnecdote(content))
+    const newAnecdote = await anecdoteService.createNewAnecdote(content)
+    dispatch(createAnecdote(newAnecdote))
   }
 
   return (
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,12 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
 
 const getId = () => (100000 * Math.random()).toFixed(0)
@@ -41,11 +35,14 @@ const anecdoteSlice = createSlice({
       sortByVotes(state)
     },
     createAnecdote(state, action) {
-      state.push(asObject(action.payload))
+      state.push(action.payload)
       sortByVotes(state)
+    },
+    setAnecdotes(state, action) {
+      return action.payload
     }
   }
 })
 
-export const { addVoteTo, createAnecdote } = anecdoteSlice.actions
+export const { addVoteTo, createAnecdote, setAnecdotes } = anecdoteSlice.actions
 export default anecdoteSlice.reducer
